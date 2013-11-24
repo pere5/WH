2c4bc0e47bf49dfb772b6842e1cba59c0bc7312d
pere5 <pere5@kth.se>
2013-11-24 16:13:31 +0100

    hey!

--- src/test/java/com/springapp/mvc/AppTests.java
+++ src/test/java/com/springapp/mvc/AppTests.java
@@ -32,8 +32,8 @@ public class AppTests {
 
     @Test
     public void simple() throws Exception {
-        mockMvc.perform(get("/"))
+        mockMvc.perform(get("/hello.htm"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("hello"));
+                .andExpect(view().name("gameScreen"));
     }
 }
